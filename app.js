4d368a1bd759a5ad2a78f90b6e6d084c559cdca9
adeeldhedhi <adeel_dhedhi@hotmail.com>
2023-08-11 21:30:58 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,276 @@
+// ----------ALERT----------
+
+// Question No-01
+alert ("Hello world!");
+
+// Question No-02
+// alert ("Error! Please enter valid password");
+
+// Question No-03
+// alert ("Welcome to Js Land \n Happy Coding!");
+
+// Question No-04
+// alert ("Welcome to JS Land");
+// alert ("Happy Coding! \n Prevent this page from creating ddional Dialogue");
+
+// Question No-05
+// console.log ("Hello... I can run JS through my web browser's console");
+// alert ("Happy Coding! \n Prevent this page from creating ddional Dialogue");
+
+// Question No-07
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <script src="app.js"></script>
+//     <title>Document</title>
+// </head>
+// <body>
+
+//     <script src="app.js"></script>
+// </body>
+// </html>
+
+// ----------VAARIBLE FOR STRINGS----------
+
+// // Question No-01
+// var usernme;
+
+// // Question No-02
+// var myName = "Mehboob Alam";
+
+// // Question No-03
+// var message;
+// var message = "Hello World";
+// alert (message);
+
+// // Question No-04
+// var usernme;
+// var age;
+// var course;
+// var usernme = "Mehboob Alam";
+// var age = 15;
+// var course = "Web and Mobile Development ";
+// alert (usernme);
+// alert (age);
+// alert (course);
+
+// // Question No-05
+// alert ("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+// // Question No-06
+// var email;
+// var email = "mehbooobalam" + "@" + "gmail" + ".com";
+// alert ("my email address is" + " " + email);
+
+// // Question No-07
+// var book;
+// var book = "A smarter way to learn JavaScript";
+// alert ("I am trying to learn from the book" + " " + book);
+
+// // Question No-08
+// document.write ("Yah! I can write HTML content thorugh JavaScript");
+
+// // Question No-08
+// document.write ("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
+
+// ----------VAARIBLE FOR NUMBERS----------
+
+// Question No-01
+// var age;
+// var age = 15;
+// alert ("I am" + " " + age + " "+"years old");
+
+// Question No-02
+// var counter;
+// var counter = 30;
+// alert ("you have visited this site" + " " + counter + " " + "times");
+
+
+// Question No-03
+// var birthYear = 1985;
+// document.write ("My birth year is" + " " + birthYear );
+
+// Question No-04
+// var visitorName = "John Doe";
+// var Tshirt = 5;
+// var companyName = "XYZ";
+// document.write(visitorName + " " + "ordered" + " " + Tshirt + " " + "T-shirts(s) on" + " " + companyName + " " + "Clothing store.");
+
+// ----------VAARIBLE NAMES: LEGAL & ILLEGAL----------
+
+// Question No-01
+var userResponse, userResponseTime, userResponseTimeLimit;
+
+// Question No-02
+// ILLEGAL
+// var user Response;
+// var @response;
+// var 1response;
+// var #response;
+// var alert;
+// LEGAL
+// var userResponse;
+// var user_Response;
+// var $userResponse;
+// var _userResponse;
+// var userAlert;
+
+// Question No-03
+// document.write("<h1> Rules for naming JS variables</h1> <br> Variables can only conain, number, $ and _. For example: $my_1stVariable <br> Variables must begin with a $ or. For example $name, _name or name <br> Variable names are case sensitive <br> Variable names should not be JS Keywords");
+
+// ----------MATH EXPRESSIONS----------
+
+// // Question No-01
+// var num = 10;
+// var anotherNum = 1;
+// var popularNumber = num + anotherNum;
+// document.write("Sum of" + " " + num + " " + "and" + " " + anotherNum + " " + "is" + " " + popularNumber);
+    
+// // Question No-02
+//    // Subtrction
+// var num = 10;
+// var anotherNum = 1;
+// var popularNumber = num - anotherNum;
+// document.write("Subtrction of" + " " + num + " " + "and" + " " + anotherNum + " " + "is" + " " + popularNumber);
+//    // Multipliction
+// var num = 10;
+// var anotherNum = 1;
+// var popularNumber = num * anotherNum;
+// document.write("Multipliction of" + " " + num + " " + "and" + " " + anotherNum + " " + "is" + " " + popularNumber);
+//     // Division
+// var num = 10;
+// var anotherNum = 1;
+// var popularNumber = num / anotherNum;
+// document.write("Division of" + " " + num + " " + "and" + " " + anotherNum + " " + "is" + " " + popularNumber);
+//     // modulus
+// var num = 10;
+// var anotherNum = 3;
+// var popularNumber = num % anotherNum;
+// document.write("modulus of" + " " + num + " " + "and" + " " + anotherNum + " " + "is" + " " + popularNumber);
+
+// // Question No-03
+
+ // var myVariable;
+ // document.write("Value after variable declaration is: " + myVariable + "<br>");
+ // myVariable = 5;
+ // document.write("Initial value: " + myVariable + "<br>");
+ // myVariable++;
+// document.write("Value after increment is: " + myVariable + "<br>");
+ // myVariable += 7;
+ // document.write("Value after addition is: " + myVariable + "<br>");
+ // myVariable--;
+ // document.write("Value after decrement is: " + myVariable + "<br>");
+ // var remainder = myVariable % 3;
+ // document.write("The remainder is: " + remainder + "<br>");
+ 
+  // // Question No-04
+// var ticketPrice = 600; 
+// var numberOfTickets = 5;
+// var totalCost = ticketPrice * numberOfTickets;
+// document.write("Total Cost to buy " + numberOfTickets + " tickets is: " + totalCost + " PKR");
+
+// // Question No-05
+// var number = 4; 
+// document.write("<h2>Multiplication Table of " + number + "</h2>");
+// for (var i = 1; i <= 10; i++) {
+//     var result = number * i;
+//     document.write(number + " x " + i + " = " + result + "<br>");
+// }
+
+// // Question No-06
+// var celsiusTemperature = 25;
+// var fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+// document.write(celsiusTemperature + "°C is " + fahrenheitTemperature + "°F<br>");
+    
+// var fahrenheitTemperature2 = 77; 
+// var celsiusTemperature2 = (fahrenheitTemperature2 - 32) * 5/9;
+// document.write(fahrenheitTemperature2 + "°F is " + celsiusTemperature2 + "°C");
+
+// // Question No-07
+// var priceItem1 = 650; 
+// var priceItem2 = 100; 
+// var quantityItem1 = 3; 
+// var quantityItem2 = 7; 
+// var shippingCharges = 100; 
+
+// var totalCostItem1 = priceItem1 * quantityItem1;
+// var totalCostItem2 = priceItem2 * quantityItem2;
+
+// var subtotal = totalCostItem1 + totalCostItem2;
+// var totalCost = subtotal + shippingCharges;
+
+// document.write("<h2>Shopping Cart</h2>");
+// document.write("Price of item 1: " + priceItem1 + " PKR<br>");
+// document.write("quantity of item 1: " + quantityItem1 + "<br>");
+// document.write("Total cost of item 1: " + totalCostItem1 + " PKR<br><br>");
+    
+// document.write("Price of item 2: " + priceItem2 + " PKR<br>");
+// document.write("quantity of item 2: " + quantityItem2 + "<br>");
+// document.write("Total cost of item 2: " + totalCostItem2 + " PKR<br><br>");
+
+// document.write("Subtotal: " + subtotal + " PKR<br>");
+// document.write("Shipping charges: " + shippingCharges + " PKR<br>");
+// document.write("<b>Total cost of your order: " + totalCost + " PKR</b>");
+
+// // Question No-08
+// var totalMarks = 980;
+// var marksObtained = 804;
+
+// var percentage = (marksObtained / totalMarks) * 100;
+
+// document.write("<h2>Percentage Calculator</h2>");
+// document.write("Total Marks: " + totalMarks + "<br>");
+// document.write("Marks Obtained: " + marksObtained + "<br>");
+// document.write("Percentage: " + percentage.toFixed(2) + "%");
+
+// // Question No-09
+// var usDollars = 10;
+// var saudiRiyals = 25;
+// var exchangeRateUSD = 104.80; 
+// var exchangeRateSAR = 28;     
+
+// var totalRupees = (usDollars * exchangeRateUSD) + (saudiRiyals * exchangeRateSAR);
+
+// document.write("<h2>Currency in PKR</h2>");
+// document.write(usDollars + " US Dollars = " + (usDollars * exchangeRateUSD) + " Pakistani Rupees<br>");
+// document.write(saudiRiyals + " Saudi Riyals = " + (saudiRiyals * exchangeRateSAR) + " Pakistani Rupees<br>");
+// document.write("Total in Pakistani Rupees: " + totalRupees.toFixed(2) + " PKR");
+
+// // Question No-10
+// var initialNumber = 7;
+
+// var result = (((initialNumber + 5) * 10) / 2);
+
+// document.write("<h2>Arithmetic Operations</h2>");
+// document.write("Initial Number: " + initialNumber + "<br>");
+// document.write("Result after arithmetic operations: " + result);
+
+// // Question No-12
+// var radius = 20;
+    
+// var circumference = 2 * Math.PI * radius;
+// var area = Math.PI * radius * radius;
+
+// document.write("<h2>Circle Properties Calculator</h2>");
+// document.write("Radius: " + radius + "<br>");
+// document.write("The circumference is " + circumference.toFixed(2) + "<br>");
+// document.write("The area is " + area.toFixed(2));
+
+// Question No-13
+var favoriteSnack = "Chocolate chip"; 
+var currentAge = 39; 
+var maximumAge = 80; 
+var amountPerDay = 3; 
+
+var yearsLeft = maximumAge - currentAge;
+var totalAmount = yearsLeft * 365 * amountPerDay;
+
+document.write("<h2>The Lifetime Supply Calculator</h2>");
+document.write("Favorite Snack: " + favoriteSnack + "<br>");
+document.write("Current Age: " + currentAge + "<br>");
+document.write("Estimated Maximum Age: " + maximumAge + "<br>");
+document.write("Amount of snacks Per Day: " + amountPerDay + " " + favoriteSnack + "(s)<br>");
+document.write("You will need " + totalAmount + " " + favoriteSnack + "(s) to last you until the ripe old age of " + maximumAge);
